133948754bbdea18f051c620998da9a42acab660
takujifunao <takuji.funao@gmail.com>
2015-09-25 17:17:00 +0900

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by takuji funao on 2015/09/25.
+//  Copyright (c) 2015年 takuji funao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by takuji funao on 2015/09/25.
+//  Copyright (c) 2015年 takuji funao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
